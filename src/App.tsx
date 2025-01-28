31f1e998868326f8ff7d78f673a9fbd4a28caedb
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2025-01-28 22:30:12 +0100

    portfolio init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Hello box
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
